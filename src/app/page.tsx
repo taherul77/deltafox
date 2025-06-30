66b88f09c5e9ad58ceb3a6732f3e4c5076a96341
SK Taherul Islam Taz <taherul15-2317@diu.edu.bd>
2025-06-30 11:25:56 +0600

    update home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import Header from "@/components/layout/Header";
-import Footer from "@/components/layout/Footer";
+
 import HeroSection from "@/components/sections/HeroSection";
 import AboutSection from "@/components/sections/AboutSection";
 import ServicesSection from "@/components/sections/ServicesSection";
