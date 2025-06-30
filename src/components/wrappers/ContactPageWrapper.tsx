bb2ddd35a69c10a80835223260adcf99689e6ab8
SK Taherul Islam Taz <taherul15-2317@diu.edu.bd>
2025-06-30 11:32:21 +0600

    fixed contact page

--- src/components/wrappers/ContactPageWrapper.tsx
+++ src/components/wrappers/ContactPageWrapper.tsx
@@ -1,20 +1,17 @@
 'use client';
 
 import React from 'react';
-import Header from '../layout/Header';
-import Footer from '../layout/Footer';
+
 import ContactBanner from '../banners/ContactBanner';
 import ContactSection from '../sections/ContactSection';
 
 const ContactPageWrapper = () => {
   return (
     <>
-      <Header />
-      <main>
+  
         <ContactBanner />
         <ContactSection />
-      </main>
-      <Footer />
+    
     </>
   );
 };
